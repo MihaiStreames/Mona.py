b2045081617341534281653bf1968eafe4ad5658
Mihai <72852703+MihaiStreames@users.noreply.github.com>
2022-12-12 12:55:27 +0100

    uhh

--- Mona.py/bot.py
+++ Mona.py/bot.py
@@ -16,6 +16,7 @@ TARGET_CHANNEL_ID = 1050924693626036334
 @client.event
 async def on_message(message):
     if message.content.startswith("!gif"):
+        # Check if message has an image
         if message.attachments:
             # Open the image file and convert it to a GIF
             image = Image.open(message.attachments[0])
@@ -32,29 +33,27 @@ async def on_raw_reaction_add(payload):
     if payload.emoji.name == '⭐':
         # Fetch the message and channel objects from the payload
         message = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
-        reaction = client.get(message.reactions)
         target_channel = client.get_channel(TARGET_CHANNEL_ID)
 
-        if reaction.count > 1:
-            # Create an embed object with the message content and link to message
-            embed = discord.Embed(description=message.content)
-            embed.set_author(name=message.author.display_name, icon_url=message.author.avatar)
-            embed.description = f"{message.content}\n {message.jump_url}"
+        # Create an embed object with the message content and link to message
+        embed = discord.Embed(description=message.content)
+        embed.set_author(name=message.author.name, icon_url=message.author.avatar)
+        embed.description = f"{message.content}\n {message.jump_url}"
 
-            # Check if the message has any attached images or videos
-            if message.attachments:
-                # If there are multiple attachments, use the first one
-                attachment = message.attachments[0]
-                # Check if the attachment is an image or video
-                if attachment.height:
-                    # If it is an image, set it as the embed image
-                    embed.set_image(url=attachment.url)
-                elif attachment.size:
-                    # If it is a video, set it as the embed video
-                    embed.add_field(name="Video", value=f"[Click here]({attachment.url}) to view the video.")
+        # Check if the message has any attached images or videos
+        if message.attachments:
+            # If there are multiple attachments, use the first one
+            attachment = message.attachments[0]
+            # Check if the attachment is an image or video
+            if attachment.height:
+                # If it is an image, set it as the embed image
+                embed.set_image(url=attachment.url)
+            elif attachment.size:
+                # If it is a video, set it as the embed video
+                embed.add_field(name="Video", value=f"[Click here]({attachment.url}) to view the video.")
 
-            # Send the embed in the target channel
-            await target_channel.send(embed=embed)
+        # Send the embed in the target channel
+        await target_channel.send(embed=embed)
 
 
 # Start the Discord client
