5c0e1b781905aeb8816b21b08a18f27013cf58d6
Mihai <moreliciouscod@gmail.com>
2022-12-13 21:20:26 +0100

    this is annoying i'll make it async properly later

--- Mona.py/bot.py
+++ Mona.py/bot.py
@@ -15,18 +15,16 @@ client = discord.Client(command_prefix='!', intents=intents)
 @client.event
 async def on_message(message):
     if message.content.startswith("!gif"):
-        # Check if message has an image
         if message.attachments:
-            # Open the image file
             image = Image.open(message.attachments[0])
-            # Convert to GIF
             image = image.convert("RGB").convert("P", palette=Image.ADAPTIVE)
+            
             # Add overlay
             draw = ImageDraw.Draw(image)
             font = ImageFont.truetype("arial.ttf", 36)
             draw.text((10, 10), "lol", font=font, fill=(255, 0, 0))
             image.save("converted.gif")
-            # Send the converted GIF back
+            
             await message.channel.send(file=discord.File("converted.gif"))
 
 @client.event
@@ -37,32 +35,49 @@ async def on_member_join(member):
 
 @client.event
 async def on_raw_reaction_add(payload):
-    # Check if the reaction is the star emoji
     if payload.emoji.name == '⭐':
-        # Fetch the message and channel objects from the payload
         message = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
         target_channel = client.get_channel(1050924693626036334)
 
-        # Create an embed object with the message content and link to message
         embed = discord.Embed(description=message.content)
         embed.set_author(name=message.author.name, icon_url=message.author.avatar)
         embed.description = f"{message.content}\n {message.jump_url}"
 
-        # Check if the message has any attached images or videos
         if message.attachments:
-            # If there are multiple attachments, use the first one
             attachment = message.attachments[0]
-            # Check if the attachment is an image or video
-            if attachment.height:
-                # If it is an image, set it as the embed image
+
+            if attachment.height: #Image
                 embed.set_image(url=attachment.url)
-            elif attachment.size:
-                # If it is a video, set it as the embed video
+
+            elif attachment.size: #Video
                 embed.add_field(name="Video", value=f"[Click here]({attachment.url}) to view the video.")
 
-        # Send the embed in the target channel
         await target_channel.send(embed=embed)
 
+@client.event
+async def on_message(message):
+    if message.content == "!reaction":
+        await message.add_reaction("🔒")
+        role = discord.utils.get(message.guild.roles, name="ba1")
+
+        # Wait for a user to react to the message
+        def check(reaction, user):
+            return user != client.user and str(reaction.emoji) == "🔒"
+        
+        reaction, user = await client.wait_for("reaction_add", check=check)
+
+        if role not in user.roles:
+            await user.add_roles(role)
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    message = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
+
+    if payload.emoji.name == "🔒":
+        role = discord.utils.get(message.guild.roles, name="ba1")
+        user = message.guild.get_member(payload.user_id)
+
+        await user.remove_roles(role)
 
 # Start the Discord client
 client.run(DISCORD_TOKEN)
